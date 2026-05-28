76a21dd8b1cd24239ee7de633a0c75cfac067e2f
Łukasz Słysz <42926373+vipluk@users.noreply.github.com>
2026-05-28 09:18:06 +0200

    Usuniecie zbednych plikow pomocniczych i katalogu demo

--- javafx-run-pracodawca.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "LOCAL_MVN=%~dp0tools\maven\bin\mvn.cmd"
-echo Uruchamiam Panel Pracodawcy przy uzyciu lokalnego Mavena...
-"%LOCAL_MVN%" javafx:run -Djavafx.mainClass=org.example.PracodawcaApp
-if %errorlevel% neq 0 pause
