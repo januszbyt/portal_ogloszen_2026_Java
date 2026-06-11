1154cc79ded9fff2786030fae795f378b16da7fc
Yurii Humennyi <urag198@gmail.com>
2026-06-11 12:42:04 +0200

    Zwiększenie responsywności interfejsu oraz dodanie komponentu ScrollPanel do okien logowania i rejestracji w celu poprawy użyteczności aplikacji na różnych rozdzielczościach ekranu.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module org.example {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive java.sql; //  Wymagane do połączenia z bazą danych
+    requires javafx.controls;           
+    requires javafx.fxml;                
+    
+    requires transitive javafx.graphics; // Do obsługi głównego okna aplikacji 
+    requires transitive javafx.base;     // Do obsługi akcji i zdarzeń 
+    
+    requires transitive java.sql;        // Do połączenia z bazą danych (SQL)
 
-    opens org.example to javafx.fxml;
-    exports org.example;
+    opens org.example to javafx.fxml;    
+    exports org.example;                
 }
