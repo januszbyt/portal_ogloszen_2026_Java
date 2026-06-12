52c583d26f94f1b3247db05cc4fee523d15a82f6
Łukasz Słysz <42926373+vipluk@users.noreply.github.com>
2026-06-12 13:19:39 +0200

    Poprawa ukladu filtrow oraz zapobieganie elipsowaniu tekstu w naglowku wyszukiwarki

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,27 +16,28 @@ import java.util.prefs.Preferences; // DODANO: Import do trwałego zapisywania u
 public class App extends Application {
 
     private static Scene scene;
-    
+
     // Inicjalizacja pamięci ustawień dla naszej aplikacji
     private static final Preferences prefs = Preferences.userNodeForPackage(App.class);
-    
-    // Teraz na starcie pobieramy zapisany stan (domyślnie false, jeśli to pierwsze uruchomienie)
+
+    // Teraz na starcie pobieramy zapisany stan (domyślnie false, jeśli to pierwsze
+    // uruchomienie)
     public static boolean isDarkMode = prefs.getBoolean("isDarkMode", false);
 
     @Override
     public void start(Stage stage) throws IOException {
         // Zwiększamy domyślny rozmiar początkowy na 800x600
         scene = new Scene(loadFXML("login"), 800, 600);
-        
+
         // Aplikowanie motywu przy starcie
         applyTheme(scene);
 
         stage.setScene(scene);
         stage.setTitle("System Ofert Pracy");
         stage.setMinWidth(600);
-        stage.setMinHeight(400);
-        
-         // Dodanie LogoIcon do naszego programu
+        stage.setMinHeight(500);
+
+        // Dodanie LogoIcon do naszego programu
         try {
             // Ścieżka wskazuje na folder resources/org/example/pictures/
             Image applicationIcon = new Image(getClass().getResourceAsStream("/org/example/pictures/LogoIcon.png"));
@@ -46,11 +47,10 @@ public class App extends Application {
         } catch (Exception e) {
             System.err.println("Wystąpił błąd podczas ładowania ikony: " + e.getMessage());
         }
-        
 
         // Ta linijka sprawia, że okno otwiera się na cały ekran (maksymalizacja)
-        stage.setMaximized(true); 
-        
+        stage.setMaximized(true);
+
         stage.show();
     }
 
@@ -67,10 +67,12 @@ public class App extends Application {
 
     // Metoda do aplikacji CSS do korzenia (root) sceny
     public static void applyTheme(Scene currentScene) {
-        // Zapisywanie wybranego motywu do trwałej pamięci systemowej przy każdej jego zmianie
+        // Zapisywanie wybranego motywu do trwałej pamięci systemowej przy każdej jego
+        // zmianie
         prefs.putBoolean("isDarkMode", isDarkMode);
 
-        if (currentScene == null || currentScene.getRoot() == null) return;
+        if (currentScene == null || currentScene.getRoot() == null)
+            return;
 
         if (isDarkMode) {
             if (!currentScene.getRoot().getStyleClass().contains("dark-mode")) {
